3f0e22ba33c9b0fd3d38c5745a9f6a00aae47f47
SolisClaudio <solisclaudio4@gmail.com>
2024-05-30 09:10:27 -0300

    First Commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import {  Cutive_Mono } from 'next/font/google';
+
+export const Mono = Cutive_Mono ({ 
+                                    subsets: ['latin'],
+                                    weight: ['400'],
+                                });
+
+// Rest of the code...
